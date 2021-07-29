4645362561dbc76ca59d11d2764210160c9a2313
abul-kalam-coder <75057436+abul-kalam-coder@users.noreply.github.com>
2021-07-29 05:20:03 +0300

    Add files via upload

--- bdrm.js
+++ bdrm.js
@@ -1,13 +1,13 @@
 var status="";
 img="";
+var objects=""
 function preload(){
-    img=loadImage("bdrm.jfif")
+    img=loadImage("bdrm.jpg")
 }
 function setup(){
 canvas=createCanvas(640,500);
 canvas.center();
 object_detector=ml5.objectDetector("cocossd",model_loaded);
-document.getElementById("status").innerHTML="status:Object detected";
 }
 function model_loaded (){
     status=true;
@@ -20,9 +20,23 @@ function gotResults(error,results){
 console.error(error);
     }else{
         console.log(results);
+        objects=results;
     }
 }
 
 function draw(){
     image(img,0,0,640,500 );
+    if(status != ""){
+        for(var i = 0;i<objects.length;i++){
+            fill("red");
+            stroke("red");
+            noFill();
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+percent+" % ",objects[i].x-265,objects[i].y-10);
+        rect(objects[i].x-270,objects[i].y-25,objects[i].width,objects[i].height);
+
+        }
+        console.log("rect drawn");
+    } 
+    
 }
